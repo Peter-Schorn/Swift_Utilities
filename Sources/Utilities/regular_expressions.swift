9cb879ced1064df0a5b56d591017cf3da9aeda08
Peter-Schorn <58197311+Peter-Schorn@users.noreply.github.com>
2020-04-23 19:48:58 -0500

    test

--- Sources/Utilities/regular_expressions.swift
+++ Sources/Utilities/regular_expressions.swift
@@ -7,86 +7,109 @@
 
 import Foundation
 
+
+public typealias RegexTuple = (
+    fullMatch: String, range: Range<String.Index>, groups: [String]
+)
+
 public extension String {
     
     
-        /**
-         Finds the first match for a regular expression patter in a string.
-         
-         - Parameters:
-             - pattern: Regular expression pattern.
-             - options: Regular expression options, such as .caseInsensitive
-         - Returns: A tuple containing the match and the range of the match
-           in the original string.
+    /**
+     Finds the first match for a regular expression pattern in a string.
+     
+     - Attention: Same as String.regexFindAll but only returns the first match.
+       See String.regexFindAll for a full explanation.
         
-         See also String.regexFindAll
-         */
-        func regexMatch(
-            _ pattern: String,
-            _ options: NSRegularExpression.Options = []
-        ) -> (match: String, range: Range<String.Index>)? {
-            
-            guard let regex = try? NSRegularExpression(
-                pattern: pattern, options: options
-            ) else {
-                fatalError("invalid regex")
-            }
-            
-            if let result = regex.firstMatch(in: self, range: NSMakeRange(0, self.count)) {
-                
-                let match = String(self[Range(result.range, in: self)!])
-                let range = strOpenRange(Range(result.range)!)
-                return (match: match, range: range)
-            }
-            return nil
+     */
+    func regexMatch(
+        _ pattern: String,
+        _ options: NSRegularExpression.Options = []
+    ) -> RegexTuple? {
+        
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: options) else {
+            fatalError("invalid regex")
         }
+        
+        let nsString = self as NSString
 
-    
+        
+        if let result = regex.firstMatch(
+            in: self, options: [], range: NSMakeRange(0, nsString.length)
+        ) {
+        
+            var regexTuple: RegexTuple
+            
+            regexTuple.fullMatch = nsString.substring(with: result.range(at: 0))
+            regexTuple.range = self.strOpenRange(Range(result.range)!, checkNegative: false)
+            regexTuple.groups = []
+            
+            for match in 1..<result.numberOfRanges {
+                regexTuple.groups.append(nsString.substring(with: result.range(at: match)))
+                
+            }
+            
+            return Optional(regexTuple)
+            
+        }
+        
+        
+        return nil
+    }
+
+
     /**
      Finds all matches for a regular expression pattern in a string.
      
      - Parameters:
         - pattern: Regular expression pattern.
         - options: Regular expression options, such as .caseInsensitive
-     - Returns: An array of tuples containing the match and
-       the range of the match in the original string, or nil if no matches are found
      
-     Usage:
+     - Returns: An array of tuples, each of which contains the full match,
+       the range of the full match, and an array of the capture groups.
+       Returns nil if no matches were found.
+     
+     Example Usage:
      ```
-     var text = "one, two, three"
-     if let results = text.regexFindAll(#"\w+"#, [.caseInsensitive]) {
-         for result in results {
-             print(result.match)
-             text.replaceSubrange(result.range, with: "new value")
-         }
-     }
+     
      ```
      */
     func regexFindAll(
         _ pattern: String,
         _ options: NSRegularExpression.Options = []
-    ) -> [(match: String, range: Range<String.Index>)]? {
+    ) -> [RegexTuple]? {
         
-        
-        guard let regex = try? NSRegularExpression(
-            pattern: pattern, options: options
-        ) else {
+        guard let regex = try? NSRegularExpression(pattern: pattern, options: options) else {
             fatalError("invalid regex")
         }
         
-        let results = regex.matches(
-            in: self, range: NSMakeRange(0, self.count)
+        let nsString = self as NSString
+        
+        let results  = regex.matches(
+            in: self, options: [], range: NSMakeRange(0, nsString.length)
         )
+
+        var fullMatches: [RegexTuple] = []
         
-        if results == [] { return nil }
-        
-        return results.map { result in
-            let strRange = self.strOpenRange(Range(result.range)!, checkNegative: false)
-            return (match: String(self[strRange]), range: strRange)
+        for result in results {
+            
+            var regexTuple: RegexTuple
+            regexTuple.fullMatch = nsString.substring(with: result.range(at: 0))
+            regexTuple.range = self.strOpenRange(Range(result.range)!, checkNegative: false)
+            regexTuple.groups = []
+            
+            for match in 1..<result.numberOfRanges {
+                regexTuple.groups.append(nsString.substring(with: result.range(at: match)))
+                
+            }
+            fullMatches.append(regexTuple)
+            
         }
         
+        if fullMatches.isEmpty { return nil }
+        return Optional(fullMatches)
     }
-    
+        
     
     /**
      Performs a regular expression replacement
