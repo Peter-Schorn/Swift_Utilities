5e11b529d01938691efdbbbad332ba597a80269a
Peter-Schorn <58197311+Peter-Schorn@users.noreply.github.com>
2020-04-26 16:58:12 -0500

    changed withTempDirectory to provide closure to execute to delete the directory

--- Sources/Utilities/file_system_extensions.swift
+++ Sources/Utilities/file_system_extensions.swift
@@ -23,24 +23,41 @@ public func makeFolder(
 }
 
 
+public enum DeleteOptions {
+    case afterClosure
+    case useHandler
+}
+
 /**
- Creates a temporary directory, passes the URL for it into a closure,
- and then deletes the directory after executing the closure.
+ Creates a temporary directory and passes the URL for it into a closure.
  
- When the directory is being deleted, if it can't be found because, e.g.,
- it was already deleted from within the closure,
- then this error is silently ignored.
- All other errors are propogated back to the caller.
+ Errors encountered when creating the directory are propogated back to the caller.
+ Errors encountered when deleting the directory are silently ignored.
+ 
  See `FileManager.default.url(for:in:appropriateFor:create:)` for
- a disucssion of the parameters.
+ a disucssion of the directory, domain, and url parameters, which are
+ forwarded through to that function.
+ 
+ - Parameters:
+   - deleteOptions:
+     - .afterClosure: Deletes the directory immediately after the closure
+           returns and sets `delelteClosure` to nil.
+     - .useHandler: Provides `delelteClosure`, which deletes the directory.
+           This is useful for asynchronous code.
+   - closure: Executes after the temporary directory is created.
+         If deleteOptions == .afterClosure, then `delelteClosure` is nil
+   - tempDir: Executes after the directory is created.
+   - delelteClosure: Deletes the directory when called.
+ - Returns: The URL of the directory, which may have already been deleted,
+       depending on the options specified above.
  */
 @discardableResult
 public func withTempDirectory(
    for directory: FileManager.SearchPathDirectory = .itemReplacementDirectory,
    in domain: FileManager.SearchPathDomainMask = .userDomainMask,
    appropriateFor url: URL = FileManager.default.homeDirectoryForCurrentUser,
-   delete: Bool = true,
-   closure: (URL) -> Void
+   deleteOptions: DeleteOptions = .afterClosure,
+   closure: (_ tempDir: URL, _ delelteClosure: (() -> Void)?) -> Void
 ) throws -> URL {
 
     let tempDir = try FileManager.default.url(
@@ -50,16 +67,19 @@ public func withTempDirectory(
         create: true
     )
     
-    closure(tempDir)
-    
-    if delete {
+    let deletionClosure = {
         do {
             try FileManager.default.removeItem(at: tempDir)
-        
-        } catch let e as NSError where e.code == 4 {
-            // code 4 indicates that the directory couldn't be found
-            print("WARNING: couldn't delete temp directory")
-        }
+
+        } catch { }
+    }
+    
+    switch deleteOptions {
+        case .afterClosure:
+            closure(tempDir, nil)
+            deletionClosure()
+        case .useHandler:
+            closure(tempDir, deletionClosure)
     }
     
     return tempDir
