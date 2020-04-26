7de3e4bd371dd2e687a507e0f076be40e990f346
Peter-Schorn <58197311+Peter-Schorn@users.noreply.github.com>
2020-04-26 02:53:56 -0500

    Updated runShellScript to return error

--- Sources/Utilities/macOS_utilities.swift
+++ Sources/Utilities/macOS_utilities.swift
@@ -10,15 +10,23 @@ import Foundation
 #if os(macOS)
 
 /**
- runs a shell script and returns the output with the trailing new line stripped
+ Runs a shell script and returns the output.
+ 
  - Parameters:
    - args: a list of arguments to run
    - launchPath: the path from which to launch the script. Default is /usr/bin/env
- - Returns: the output as String?
+   - stdout: Pipe for the standard output. If nil (default) then a new pipe
+        will be created.
+   - stderror: Pipe for standard error. If nil (default) then a new pipe
+        will be created.
+ - Returns: A tuple containing String? for the output and for the error.
  */
 public func runShellScript(
-    args: [String], launchPath: String = "/usr/bin/env"
-) -> String? {
+    args: [String],
+    launchPath: String = "/usr/bin/env",
+    stdout: Pipe? = nil,
+    stderror: Pipe? = nil
+) -> (stdout: String?, stderror: String?) {
     
     // Create a Task instance
     let task = Process()
@@ -27,21 +35,28 @@ public func runShellScript(
     task.launchPath = launchPath
     task.arguments = args
      
-    // Create a Pipe and make the task
-    // put all the output there
-    let pipe = Pipe()
-    task.standardOutput = pipe
+    // Create Pipes
+    let standardOutput  = stdout ?? Pipe()
+    task.standardOutput = standardOutput
+    
+    let standardError   = stderror ?? Pipe()
+    task.standardError  = standardError
 
     // Launch the task
     task.launch()
 
     // Get the data
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    let nsOutput = NSString(data: data, encoding: String.Encoding.utf8.rawValue)
-    let output = nsOutput as String?
-    return output?.strip()
+    let stdoutData = standardOutput.fileHandleForReading.readDataToEndOfFile()
+    let nsStdoutOutput = NSString(data: stdoutData, encoding: String.Encoding.utf8.rawValue)
+    let stdoutOutput = (nsStdoutOutput as String?)?.strip()
     
-
+    let stderrorData = standardError.fileHandleForReading.readDataToEndOfFile()
+    let nsStderrorOutput = NSString(data: stderrorData, encoding: String.Encoding.utf8.rawValue)
+    let stderrorOutput = (nsStderrorOutput as String?)?.strip()
+    
+    
+    return (stdout: stdoutOutput, stderror: stderrorOutput)
+    
 }
 
 #endif
